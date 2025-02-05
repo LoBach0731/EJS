d8dd44fd06fea8918ed8c1a00a1a3ffd70e8190a
Logan Bach <logan.r.bach@gmail.com>
2025-02-05 10:14:34 -0500

    fart

--- controllers/views/gamePageController.js
+++ controllers/views/gamePageController.js
@@ -1,11 +1,32 @@
 const axios = require("axios");
 const settings = require("../../config/settings")
 async function renderHomePage(req, res) {
-    let data = { game_id: req.params.id };
-    res.render("game", {
-        title: "Games!",
-        gameData: data,
-    });
+    let data = [];
+        const response = await axios.get(
+            `${settings.ROOT}:${settings.PORT}/api/games/${req.params.id}`,
+        );
+        data = response.data;
+        const cover = await getGameDataFromEndPoint(req.params.id, "covers");
+        data.cover = cover[0] ? cover[0].url : "";
 
+        const genres = await getGameDataFromEndPoint(req.params.id, "genres"); 
+        data.genres = genres;
+
+
+        console.log(data);
+        res.render("game", {
+        title: "CMP - IGDB: " + data.name,
+        gameData: data,
+        });
+}
+
+async function getGameDataFromEndPoint(gameid, endpoint) {
+    let values = await axios.get(
+        `${settings.ROOT}:${settings.PORT}/api/${endpoint}`,
+        {
+            params: { gameid: gameid },
+        }
+    );
+    return values.data;
 }
 module.exports = {renderHomePage};
